e76b27f88c9de76ca729500ece39cab7d839d20e
Philippe Aristote <philippe.aristote@gmail.com>
2013-12-31 18:17:34 +0700

    add comments

--- Website/Picss.php
+++ Website/Picss.php
@@ -1,6 +1,6 @@
 <?php
 
-
+// Picss controller
 class Picss {
 
 	public $db;
@@ -13,6 +13,8 @@ class Picss {
 		$this->db=$db;
 	}
 
+	// called when the URL /picss/@name is requested with GET
+	// get the Picss by its name and render it in a separate page
 	function get($f3, $args) {
 		$picss=new DB\Mongo\Mapper($this->db,'picss');
 		$name=empty($args['name'])?'':$args['name'];
@@ -21,6 +23,8 @@ class Picss {
 		$f3->set('inc', 'onepicss.htm');
 	}
 
+	// called when /picss is requested in GET
+	// get all Picss and render them on the front / list page
 	function getAll($f3) {
 		$picss=new DB\Mongo\Mapper($this->db,'picss');
 		$items = $picss->find();
@@ -28,8 +32,10 @@ class Picss {
 		$f3->set('inc', 'allpicss.htm');
 	}
 
+	// called when the URL /picss/@name is requested with POST
+	// create a Picss with the data sent in the request
 	function post($f3) {
-
+		// check whether name and label have been set
 		if (!$f3->exists('POST.name') || !strlen($f3->get('POST.name')))
 			$f3->set('message','Name is required');
 		elseif (!$f3->exists('POST.label') || !strlen($f3->get('POST.label')))
@@ -37,8 +43,10 @@ class Picss {
 		else {
 			$db=$this->db;
 			$newpicss=new DB\Mongo\Mapper($this->db,'picss');
+			// populate the newpicss attributes with those existing in the POST request
+			// for this to work, form fields *must* be named after the attributes of the document stored in Mongo
 			$newpicss->copyfrom('POST');
-
+			// move uploaded files to the UPLOADS dir (aka /uploads)
 			$web = \Web::instance();
 			$files = $web->receive(
 				function($file){
@@ -46,7 +54,8 @@ class Picss {
     			}
 			);
 
-
+			// if files have been uploaded and moved
+			// set their local path in the Mongo document via 2 attributes image and sound
 			if ($files && $f3->exists('FILES')) {
 				// $thumb = new Image($f3->get('FILES')['image']['name'], false, $f3->get('TMP').'/'.$f3->get('FILES')['image']['tmp_name']);
 				// $thumb->resize(150,150);
@@ -54,13 +63,16 @@ class Picss {
 				$newpicss->set('image', $f3->get('FILES')['image']['name']);
 				$newpicss->set('sound', $f3->get('FILES')['sound']['name']);
 			}
+			// save the new document in Mongo
 			$newpicss->save();
-			$f3->set('picss', $newpicss);
-			$f3->set('layoutfile','savedpicssresponse.json');
-			$f3->set('format','application/json');
-			//$f3->reroute('/picss');
+			// send the json response with the newpicss data (TODO fix the json formatting)
+			// $f3->set('picss', $newpicss);
+			// $f3->set('layoutfile','savedpicssresponse.json');
+			// $f3->set('format','application/json');
+
+			// for tests: rather than rendering json, simply reroute to the list of Picss
+			$f3->reroute('/picss');
 		}
-		//$this->getAll($f3);
 	}
 
 	function put() {
@@ -71,13 +83,17 @@ class Picss {
 
 	}
 
+	// called before any other function
 	function beforeRoute($f3) {
+		// set the layout file and its format. by default it is layout.htm in text/html format
 		$f3->set('layoutfile','layout.htm');
 		$f3->set('format','text/html');
 	}
 
+	// called after any other function
 	function afterRoute($f3) {
-		// Render HTML layout
+		// final rendering of the response with the specified layout file and format
+
 		// if (isset($f3->get('format')))
 		// 	echo Template::instance()->render('savedpicssresponse.json', 'application/json');
 		// else
--- Website/index.php
+++ Website/index.php
@@ -2,19 +2,26 @@
 
 $f3 = require('fff/base.php');
 
+// the Picss controller
+// TODO move to a separate folder with other model / controller classes
 require('Picss.php');
 
 $f3->set('UPLOADS','uploads/'); // don't forget to set an Upload directory, and make it writable!
 $f3->set('TEMP','tmp/'); // don't forget to set an Temp directory, and make it writable!
 
+// route / to /picss
 $f3->route('GET /',
 	       function($f3) {
 	       	$f3->reroute('/picss');	
 	       });
 
+// map HTTP methods to Picss methods
 $f3->map('/picss/@name','Picss');
 
+// front page
 $f3->route('GET /picss', 'Picss->getAll');
+
+// add picss
 $f3->route('POST /picss', 'Picss->post');
 
 $f3->run();
