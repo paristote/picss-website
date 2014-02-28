4e6344e57bdf635ee3c02370a332b122ccff04d3
Philippe Aristote <philippe.aristote@gmail.com>
2014-02-28 18:01:59 +0700

    store picss in data folder

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $f3->config('data/setup.cfg');
 
 
 $f3->set('AUTOLOAD', 'app/'); // loads all classes in the app/ directory
-$f3->set('UPLOADS','uploads/'); // uploaded files go here after validation
+$f3->set('UPLOADS','data/'); // uploaded files go here after validation
 $f3->set('TEMP','tmp/'); // temp directory 
 $f3->set('UI','views/'); // contains all the html views and json response formats
 
